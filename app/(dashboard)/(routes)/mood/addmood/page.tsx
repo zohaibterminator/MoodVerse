826ce5e372764a904d5c8abf31ee84288918e2dd
syedahajra <syedahajramutaal@gmail.com>
2023-12-04 23:10:12 +0500

    mood ka data going

---
+++ app/(dashboard)/(routes)/mood/addmood/page.tsx
@@ -0,0 +1,152 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import * as z from "zod";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+const formSchema = z.object({
+  mood: z.string().min(0, {
+    message: "Enter your mood",
+  }),
+  Note: z.string(),
+  intensity: z.coerce.number(),
+  location: z.string(),
+  weather: z.string(),
+});
+
+function Profile() {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post("/api/mood", values);
+      router.push(`/mood`);
+      toast.success("Success");
+    } catch {
+      toast.error("something went wrong");
+    }
+  };
+  return (
+    <div className="p-6 flex items-center justify-center ">
+      <div className="w-1/2 bg-white rounded-lg p-6">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="mood"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Mood</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="Sad,Excited,Depressed..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="Note"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Add a Note</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      disabled={isSubmitting}
+                      placeholder="The day didn’t go well in the morning. I tried to make coffee, but it burned out. I missed my bus"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="intensity"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Intensity</FormLabel>
+                  <FormControl>
+                    <Input
+                    type="number"
+                      disabled={isSubmitting}
+                      placeholder="Rate your mood intensity on a scale of 1 to 10"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Location</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="enter your Location"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="weather"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Weather</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="enter weather"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div>
+              <Button type="submit">Continue</Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
+export default Profile;
---
+++ app/api/mood/route.ts
@@ -0,0 +1,31 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST (
+    req:Request,
+){
+    try{
+        const { userId }=auth();
+        const {mood,Note,intensity,location,weather} = await req.json();
+        if(!userId){
+            return new NextResponse("Unauthorized",{status:401});
+        }
+
+        const mood_tracking = await db.mood_Tracking.create({
+            data:{
+                userId,
+                mood,
+                Note,
+                intensity,
+                location,
+                weather
+            },
+        });
+        return NextResponse.json(mood_tracking);
+    }
+    catch (error){
+        console.log("[Profile]",error);
+        return new NextResponse("Internal Error", {status:500});
+    }
+}
