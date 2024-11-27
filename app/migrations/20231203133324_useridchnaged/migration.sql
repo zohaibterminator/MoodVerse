ec077e63d3281c8eb5298fb2fc3a0ed44d962353
Zohaib Saqib <117584067+zohaibterminator@users.noreply.github.com>
2024-11-27 22:44:51 +0500

    updated code

---
+++ app/migrations/20231203133324_useridchnaged/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `User_Profile` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User_Profile" DROP COLUMN "userId";
