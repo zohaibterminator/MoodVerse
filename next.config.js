d11d29a7b91df197a0684183fcdb1f7410f45ca8
Zohaib Saqib <117584067+zohaibterminator@users.noreply.github.com>
2024-11-29 20:04:10 +0500

    whitelisted domain of blob storage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
-    output: "standalone",
-  };
+const nextConfig = {
+  output: "standalone",
+  images: {
+    domains: ["moodverse.blob.core.windows.net"], // Add your Blob Storage domain here
+  },
+};
 
 module.exports = nextConfig;
